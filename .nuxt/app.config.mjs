d650566ad0628b80546e37e01973f2b47714d4f7
loicpennequin <lo.pennequin@gmail.com>
2023-02-25 18:10:08 +0100

    remove root .nuxt directory KEKW

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from 'C:/Users/Loïc/web/create-d3-app/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-import cfg0 from "C:/Users/Loïc/web/create-d3-app/src/app.config.ts"
-
-export default defuFn(cfg0, inlineConfig)
--- .nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = string
-declare module "C:/Users/Loïc/web/create-d3-app/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
