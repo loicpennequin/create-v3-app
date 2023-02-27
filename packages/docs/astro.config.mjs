9479656e1f49c9498b77e8423f6c3d5e3d84d063
loicpennequin <lo.pennequin@gmail.com>
2023-02-27 18:47:21 +0100

    feat: table of contents wip

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -1,4 +1,20 @@
 import { defineConfig } from 'astro/config';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    plugins: [
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            class: 'heading-link heading-link--hidden---effects',
+            'data-heading-link': true
+          },
+          behavior: 'wrap'
+        }
+      ]
+    ]
+  }
+});
