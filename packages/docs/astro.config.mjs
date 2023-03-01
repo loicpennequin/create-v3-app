dbbad8503229639d54d990a7208e0edeb5311b99
loicpennequin <lo.pennequin@gmail.com>
2023-03-01 11:50:45 +0100

    fixed burger menu on firefox

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -1,16 +1,24 @@
 import { defineConfig } from 'astro/config';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import remarkCodeTitles from 'remark-code-titles';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://create-v3-app.vercel.app',
   markdown: {
-    plugins: [
+    remarkPlugins: [remarkCodeTitles],
+    rehypePlugins: [
       [
         rehypeAutolinkHeadings,
         {
+          properties: {},
           behavior: 'wrap'
         }
       ]
-    ]
+    ],
+    shikiConfig: {
+      theme: 'rose-pine',
+      wrap: true
+    }
   }
 });
---
+++ packages/docs/src/pages/docs/faq.md
@@ -0,0 +1,19 @@
+---
+title: FAQ
+description: Frequently asked questions
+layout: ../../layouts/withSidebars.astro
+lang: en
+---
+
+## FAQ
+
+```vue:hello.js
+<script setup lang="ts">
+const foo = ref(0);
+</script>
+<template>
+  <div>
+    {{ foo }}
+  </div>
+</template>
+```
